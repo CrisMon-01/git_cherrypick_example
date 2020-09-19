9448772e96668f64195508a32b20fb076bf4045d
crismon-01 <cristianmonti1995@gmail.com>
2020-09-19 13:39:26 +0200

    query 1 branch1

---
+++ example.py
@@ -0,0 +1,18 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="0.0.0.0",
+  port="6603",
+  user="root",
+  password="pass1",
+  database="db"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SELECT * FROM tab")
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
